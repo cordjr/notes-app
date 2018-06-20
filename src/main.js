47a007ecfbf27d6c72e60264eb1fa41b827b8eea
cordjr <jose.cordeiro@armateus.com.br>
2018-06-20 12:17:52 -0300

    added store imlementations

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
-
+Vue.use(Vuex)
+const store  =  new Vuex.Store({version:'1.0'})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
